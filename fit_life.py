2ea601e5ac8176df12a9e91e24c50086d17a303b
Saveliy Vasilev <sava895245@gmail.com>
2026-09-17 16:43:08 +0900

    добавил приветствие

--- fit_life.py
+++ fit_life.py
@@ -1,3 +1,4 @@
+print('Добро пожаловать в программу "Fit Life"!')
 user_name = input('Введите ваше имя: ')
 user_age = int(input('Введите ваш возраст: '))
 user_weight = float(input('Введите ваш вес (в кг): '))
